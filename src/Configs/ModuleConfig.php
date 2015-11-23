ed8ad4d154ede3b723dc95f2f57e67f43c259d65
mvenghaus <info@inklammern.de>
2015-11-23 13:11:31 +0100

    update

--- src/Configs/ModuleConfig.php
+++ src/Configs/ModuleConfig.php
@@ -5,16 +5,41 @@ namespace Inkl\Core\Configs;
 class ModuleConfig {
 
 	private $config;
+	/** @var AppConfig */
+	private $appConfig;
 
 	/**
-	 * @param $config
+	 * ModuleConfig constructor.
+	 * @param AppConfig $appConfig
 	 */
-	public function __construct($config) {
+	public function __construct(AppConfig $appConfig) {
+		$this->appConfig = $appConfig;
+
+		$this->load();
+	}
+
+	private function load() {
+
+		$file = 'config/config.php';
+		$config = [];
+		foreach (new \DirectoryIterator($this->appConfig->getModulePath()) as $info)
+		{
+			if (!$info->isDot() && $info->isDir() && file_exists($info->getRealPath() . '/' . $file))
+			{
+				$configData = require $info->getRealPath() . '/' . $file;
+
+				$config = array_merge_recursive($config, $configData);
+			}
+		}
+
 		$this->config = $config;
 	}
 
+	public function get($path = '', $default = '') {
 
-	public function get($path, $default = '') {
+		if (!$path) {
+			return $this->config;
+		}
 
 		$current = &$this->config;
 		foreach (explode('/', $path) as $key) {
